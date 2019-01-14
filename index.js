dfc37d6c4f51573d0e1d3a9f8fee8154315f8e44
Pavel Pichrt <pavel@heepo.co>
2019-01-14 15:47:10 +0000

    Added the existing config

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+	arrowParens: 'avoid',
+	printWidth: 120,
+	singleQuote: true,
+	semi: true,
+	tabWidth: 2,
+	trailingComma: 'all',
+	bracketSpacing: true,
+	useTabs: true,
+	jsxBracketSameLine: true
+};
